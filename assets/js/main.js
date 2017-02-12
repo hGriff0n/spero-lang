7531a2df7f11ccbd1aa58745a17dbb25da46f19f
hGriff0n <ghooper96@gmail.com>
2017-02-12 00:17:45 -0500

    Added Page Switching through jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 
 $(document).ready(function () {
+  /*
+   * This stuff is carried over from the Uno framework that I built this site on top of
+   */
   $('a.blog-button').click(function (e) {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-
     currentWidth = $('.panel-cover').width()
 
     if (currentWidth < 960) {
@@ -13,14 +15,7 @@ $(document).ready(function () {
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
-
-  if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
-  // if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
   $('.panel-cover').addClass('panel-cover--collapsed')
-  // }
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
@@ -31,5 +26,39 @@ $(document).ready(function () {
     $('.navigation-wrapper').toggleClass('visible')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
+
+
+  /*
+   * This section was added to implement moving pages (TODO)
+   */
+  var cur_page = $('#demo-page');
+  var cur_btn = $('#demo');
+
+  var addSwitchClick = function(btn, page) {
+    btn.click(function() {
+      if (!btn.hasClass('vis-pg')) {
+        cur_page.hide();
+        page.show();
+
+        cur_btn.removeClass('vis-pg');
+        btn.addClass('vis-pg');
+
+        cur_page = page;
+        cur_btn = btn;
+      }
+    })
+  }
+
+  var demo = $('#demo');
+  demo.addClass('vis-pg');
+  addSwitchClick(demo, $('#demo-page'));
+
+  var feat_page = $('#feat-page');
+  feat_page.hide();
+  addSwitchClick($('#feat'), feat_page);
+
+  var tutr_page = $('#tutr-page');
+  tutr_page.hide();
+  addSwitchClick($('#tutr'), tutr_page);
 
 })
