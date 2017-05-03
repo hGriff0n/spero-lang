4a316f57700d5beefca64794bdfcee36df11dac8
hGriff0n <ghooper96@gmail.com>
2017-05-02 21:25:14 -0400

    Added Demo Programs
    Adds a list (with css highlighting) next to the demo box providing a bunch of "sample" programs for testing
    Adds js callbacks to fill the editor with the sample program text when clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,3 +62,151 @@ $(document).ready(function () {
   addSwitchClick($('#tutr'), tutr_page);
 
 })
+
+function calculatorDemo() {
+  $("#demo-list > li").removeClass('curr-list-item');
+  $("#sc").addClass('curr-list-item');
+
+  ace.edit("editor").session.setValue(`# This code is editable and runnable!
+mod spero:demo
+
+def main = () -> {
+    # A simple integer calculator:
+    # \`+\` or \`-\` means add or subtract by 1
+    # \`*\` or \`/\` means multiply or divide by 2
+
+    let program = "+ + * - /";
+    let acc = mut 0;
+
+    for char in program do
+        char.match {
+            case '+' -> acc += 1
+            case '-' -> acc -= 1
+            case '*' -> acc *= 2
+            case '/' -> acc /= 2
+            case _ -> ()
+        }
+
+    "The program \"{}\" evaluates to {}"
+        .std:io:println(program, acc)
+}`);
+}
+
+function helloWorldDemo() {
+  $("#demo-list > li").removeClass('curr-list-item');
+  $("#hw").addClass('curr-list-item');
+
+  ace.edit("editor").session.setValue(`use std:io:println as puts
+def main = () -> "Hello, World!".puts
+
+## println gets rebound to the name \`puts\` to cut down
+on the typing, although that's not explicitly needed ##`)
+}
+
+function palindromeDemo() {
+  $("#demo-list > li").removeClass('curr-list-item');
+  $("#ep").addClass('curr-list-item');
+
+  ace.edit("editor").session.setValue(`# Determine whether the given number is a palindrome
+def palindrome? = (n :: Size) -> {
+    let n = mut n.toStr.split(n.len / 2)
+
+    # Extend the string if the original had odd length
+    if n(0).size != n(1).size
+      n(0) += n(1)(0)
+
+    # Compare the two substrings against each other
+    n(0).zipWith(n(1).reverse).reduce(
+        (b, a) -> b && a.0 == a.1, true)
+}
+
+# Find the largest palindrome made by multiplying 3-digit numbers
+def main = () -> {
+  let max = 999 * 999
+  let min = 111 * 111
+
+  "{}".std:io:println({max..min}.findIf(palindrome?))
+}`);
+}
+
+function insertionSortDemo() {
+  $("#demo-list > li").removeClass('curr-list-item');
+  $("#is").addClass('curr-list-item');
+
+  ace.edit("editor").session.setValue(`def insertionSort[T :: Comparable] =
+  (list :: mut Indexable[T, Size]&) -> {
+    ## Note: I'm not sure yet whether \`impl\` should be
+     able to capture the variable \`list\` as list is a
+     mutable reference (lifetime issues in some cases) ##
+    def impl = (l :: Size, r :: Size) -> {
+        # Partition the array
+        for i in 1..{r - 1} do {
+          let min = mut j;
+          for i in {j + 1}..list.size
+            if list(i) < list(min) min = i
+        }
+
+        # Swap the partition if necessary
+        list.swap(i, j).if min != j
+    }
+
+    list.insertionSort(0, list.size)
+}
+
+def main = () ->
+    [ 3, 2, 5, 4 ].insertionSort.map("{}".std:io:println)`);
+}
+
+function erasthosenesDemo() {
+  $("#demo-list > li").removeClass('curr-list-item');
+  $("#se").addClass('curr-list-item');
+
+  ace.edit("editor").session.setValue(`# Basic Sieve of Erasthosenes
+let genPrimes = (max :: Size) ->{
+    static primes = mut [2].Vector[Size]
+
+    let cur_prime = mut primes.back
+    while cur_prime < max {
+        # A bit inefficient for the moment
+        cur_prime += 1
+        let is_prime = primes.reduce(
+            (b, p) -> b || cur_prime % p == 0, false)
+
+        primes.pushBack(cur_prime).if is_prime
+    }
+
+    return primes
+}
+
+# Show all primes smaller than 100
+def main = () -> "{}".std:io:println(genPrimes(100))`);
+}
+
+function customTypeDemo() {
+  $("#demo-list > li").removeClass('curr-list-item');
+  $("#ct").addClass('curr-list-item');
+
+  ace.edit("editor").session.setValue(`use std:io:*
+
+def Foo = (s :: String) {
+    let count = mut 0
+    "{} is a baz!".println(s)
+
+    # This is a mutable method
+    def inc = mut () -> count += 1
+    def performBar = () ->
+        "This is not a mutable method!".println
+
+    def drop = mut () -> "Incremented {} times!".println(count)
+}
+
+# Prints "Carson is a baz!"
+# Prints "Incremented 2 times!"
+def main = () -> {
+    let f = mut "Carson".Foo
+    f.inc; f.inc
+
+    let f = f
+    f.performBar
+}`);
+}
